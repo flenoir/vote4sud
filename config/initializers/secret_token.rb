9efd4a7154bbc6e247bf2f15f352efb5f9b0f2c1
fabien lenoir <flenoir@gmail.com>
2014-01-14 18:14:33 +0100

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vote4sud::Application.config.secret_key_base = 'db190b00fbf42dcb2be926425d5af04bcce0d0134bf53fa5f1bc3a869696e2b17568f38563dde0f8e0a5e20299a1129ec8a3a432b3a840783583b15ee0466d90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vote4sud::Application.config.session_store :cookie_store, key: '_vote4sud_session'
